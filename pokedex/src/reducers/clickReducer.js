080c3939e236b7942f3c16ba5ac55349eafc1203
Débora Teodoríco <debora.steodorico@gmail.com>
2021-10-15 07:38:13 -0300

    refactor: correcting indentation

--- pokedex/src/reducers/clickReducer.js
+++ pokedex/src/reducers/clickReducer.js
@@ -2,18 +2,17 @@ import { CLICK_UPDATE_VALUE } from '../actions/actionsTypes';
 
 const initialState = {
     newValue: ''
-  };
+};
 export const clickReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case CLICK_UPDATE_VALUE:
-        return {
-          ...state,
-          newValue: action.newValue
-        };
-      default:
-        return state;
-    }
-  };
+  switch (action.type) {
+    case CLICK_UPDATE_VALUE:
+      return {
+        ...state,
+        newValue: action.newValue
+      };
+    default:
+      return state;
+  }
+};
 
 export default clickReducer;
-
