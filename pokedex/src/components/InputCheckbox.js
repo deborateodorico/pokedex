ac01936b45ed4c8964396f0632d5f9b04807120e
Débora Teodoríco <debora.steodorico@gmail.com>
2021-10-22 16:41:48 -0300

    refactor: componentizing checkbox w

--- pokedex/src/components/InputCheckbox.js
+++ pokedex/src/components/InputCheckbox.js
@@ -1,29 +1,29 @@
 import React from 'react';
 
-export default function InputCheckbox({Weigths, heights, checkboxWeigths, checkboxHeights}) {
+export default function InputCheckbox({weigths, heights, checkboxWeigths, checkboxHeights}) {
     return (
         <div>
         <div>
           <p>weights</p>
           <label>
             1
-            <input type="checkbox" name="pokemon-weigths" value="1" checked={Weigths === "1"} onChange={checkboxWeigths}/>
+            <input type="checkbox" name="pokemon-weigths" value="1" checked={weigths === "1"} onChange={checkboxWeigths}/>
           </label>
           <label>
             2
-            <input type="checkbox" name="pokemon-weigths" value="2" checked={Weigths === "2"} onChange={checkboxWeigths}/>
+            <input type="checkbox" name="pokemon-weigths" value="2" checked={weigths === "2"} onChange={checkboxWeigths}/>
           </label>
           <label>
             3
-            <input type="checkbox" name="pokemon-weigths" value="3" checked={Weigths === "3"} onChange={checkboxWeigths}/>
+            <input type="checkbox" name="pokemon-weigths" value="3" checked={weigths === "3"} onChange={checkboxWeigths}/>
           </label>
           <label>
             4
-            <input type="checkbox" name="pokemon-weigths" value="4" checked={Weigths === "4"} onChange={checkboxWeigths}/>
+            <input type="checkbox" name="pokemon-weigths" value="4" checked={weigths === "4"} onChange={checkboxWeigths}/>
           </label>
           <label>
             5
-            <input type="checkbox" name="pokemon-weigths" value="5" checked={Weigths === "5"} onChange={checkboxWeigths}/>
+            <input type="checkbox" name="pokemon-weigths" value="5" checked={weigths === "5"} onChange={checkboxWeigths}/>
           </label>
         </div>
         <div>
