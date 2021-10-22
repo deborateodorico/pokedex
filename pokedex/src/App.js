fc75d08c7ad5e5c7951fca14b3ba009269f61d34
Débora Teodoríco <debora.steodorico@gmail.com>
2021-10-22 16:00:50 -0300

    refactor: componentizing checkbox

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { clickButton } from './actions';
 import Pokedex from './components/Pokedex';
+import InputCheckbox from './components/InputCheckbox';
 import './index.scss';
 
 function App(props) {
@@ -56,54 +57,13 @@ function App(props) {
     
   return (
     <div className="App" style={{ paddingTop: '10px' }}>
-      <div>
-        <div>
-          <p>weights</p>
-          <label>
-            1
-            <input type="checkbox" name="pokemon-weigths" value="1" checked={checkboxWeigths === "1"} onChange={hancleChangeCheckboxWeigths}/>
-          </label>
-          <label>
-            2
-            <input type="checkbox" name="pokemon-weigths" value="2" checked={checkboxWeigths === "2"} onChange={hancleChangeCheckboxWeigths}/>
-          </label>
-          <label>
-            3
-            <input type="checkbox" name="pokemon-weigths" value="3" checked={checkboxWeigths === "3"} onChange={hancleChangeCheckboxWeigths}/>
-          </label>
-          <label>
-            4
-            <input type="checkbox" name="pokemon-weigths" value="4" checked={checkboxWeigths === "4"} onChange={hancleChangeCheckboxWeigths}/>
-          </label>
-          <label>
-            5
-            <input type="checkbox" name="pokemon-weigths" value="5" checked={checkboxWeigths === "5"} onChange={hancleChangeCheckboxWeigths}/>
-          </label>
-        </div>
-        <div>
-          <p>heights</p>
-          <label>
-            1
-            <input type="checkbox" name="pokemon-heights" value="1" checked={checkboxHeights === "1"} onChange={hancleChangeCheckboxHeights}/>
-          </label>
-          <label>
-            2
-            <input type="checkbox" name="pokemon-heights" value="2" checked={checkboxHeights === "2"} onChange={hancleChangeCheckboxHeights}/>
-          </label>
-          <label>
-            3
-            <input type="checkbox" name="pokemon-heights" value="3" checked={checkboxHeights === "3"} onChange={hancleChangeCheckboxHeights}/>
-          </label>
-          <label>
-            4
-            <input type="checkbox" name="pokemon-heights" value="4" checked={checkboxHeights === "4"} onChange={hancleChangeCheckboxHeights}/>
-          </label>
-          <label>
-            5
-            <input type="checkbox" name="pokemon-heights" value="5" checked={checkboxHeights === "5"} onChange={hancleChangeCheckboxHeights}/>
-          </label>
-        </div>
-      </div>
+      <InputCheckbox
+        weigths={checkboxWeigths}
+        heights={checkboxHeights}
+        checkboxWeigths={hancleChangeCheckboxWeigths}
+        checkboxHeights={hancleChangeCheckboxHeights}
+        />
+     
       <button type="submit" onClick={hadleSubmitButton}>Submit</button>
       <Pokedex pokemons={pokemons} />
     </div>
---
+++ pokedex/src/components/InputCheckbox.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+export default function InputCheckbox({Weigths, heights, checkboxWeigths, checkboxHeights}) {
+    return (
+        <div>
+        <div>
+          <p>weights</p>
+          <label>
+            1
+            <input type="checkbox" name="pokemon-weigths" value="1" checked={Weigths === "1"} onChange={checkboxWeigths}/>
+          </label>
+          <label>
+            2
+            <input type="checkbox" name="pokemon-weigths" value="2" checked={Weigths === "2"} onChange={checkboxWeigths}/>
+          </label>
+          <label>
+            3
+            <input type="checkbox" name="pokemon-weigths" value="3" checked={Weigths === "3"} onChange={checkboxWeigths}/>
+          </label>
+          <label>
+            4
+            <input type="checkbox" name="pokemon-weigths" value="4" checked={Weigths === "4"} onChange={checkboxWeigths}/>
+          </label>
+          <label>
+            5
+            <input type="checkbox" name="pokemon-weigths" value="5" checked={Weigths === "5"} onChange={checkboxWeigths}/>
+          </label>
+        </div>
+        <div>
+          <p>heights</p>
+          <label>
+            1
+            <input type="checkbox" name="pokemon-heights" value="1" checked={heights === "1"} onChange={checkboxHeights}/>
+          </label>
+          <label>
+            2
+            <input type="checkbox" name="pokemon-heights" value="2" checked={heights === "2"} onChange={checkboxHeights}/>
+          </label>
+          <label>
+            3
+            <input type="checkbox" name="pokemon-heights" value="3" checked={heights === "3"} onChange={checkboxHeights}/>
+          </label>
+          <label>
+            4
+            <input type="checkbox" name="pokemon-heights" value="4" checked={heights === "4"} onChange={checkboxHeights}/>
+          </label>
+          <label>
+            5
+            <input type="checkbox" name="pokemon-heights" value="5" checked={heights === "5"} onChange={checkboxHeights}/>
+          </label>
+        </div>
+      </div>
+    )
+}
