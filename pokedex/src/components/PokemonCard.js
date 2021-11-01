b93bd2b6078dae9a6cc9a20c86cfc52f757b5516
Débora Teodoríco <debora.steodorico@gmail.com>
2021-11-01 16:05:34 -0300

    refactor: add changes

--- pokedex/src/components/PokemonCard.js
+++ pokedex/src/components/PokemonCard.js
@@ -6,17 +6,17 @@ import colors from './colorsDictionary';
 export default function PokemonCard({pokemon}) {
   return (
     <div className="pokemon" style={{border: colors[pokemon.type[0]].borderPokemon}}>
-      <div className="pokemon-infos" style={{background: colors[pokemon.type[0]].background }}>
-        <img src={Plus} alt="plus-img" className="plus-icon" />
-        <img src={Heart} alt="heart-img" className="heart-icon" />
-        <p className="pokemon-id">#{("0000" + pokemon.id).slice(-4)}</p>
-        <img src={pokemon.picture} alt={pokemon.name}/>
-        <p className="pokemon-name">{pokemon.name}</p>
+      <div className="pokemon__infos" style={{background: colors[pokemon.type[0]].background }}>
+        <img src={Plus} alt="plus-img" className="pokemon__infos__plus-icon" />
+        <img src={Heart} alt="heart-img" className="pokemon__infos__heart-icon" />
+        <p className="pokemon__infos__id">#{("0000" + pokemon.id).slice(-4)}</p>
+        <img className="pokemon__infos__img"src={pokemon.picture} alt={pokemon.name}/>
+        <p className="pokemon__infos__name">{pokemon.name}</p>
       </div>
-      <div className="pokemon-type-wrapper">
-        <div className="pokemon-type">{pokemon.type.map((item) => {
-          return <p key={pokemon.id} className="pokemon-type-paragraph" style={{border: colors[item].borderPokemon}}>{item}</p>
-        })}</div>
+      <div className="pokemon__type__wrapper">
+        {pokemon.type.map((item) => {
+          return <p key={pokemon.id} className="pokemon__type__paragraph" style={{border: colors[item].borderPokemon}}>{item}</p>
+        })}
       </div>
     </div>
   )
