d0986398095d7c2101103ce61e63482d2bfcf20f
Débora Teodoríco <debora.steodorico@gmail.com>
2021-11-02 11:27:57 -0300

    refactor: add changes to the css

--- pokedex/src/components/PokemonCardInfos.js
+++ pokedex/src/components/PokemonCardInfos.js
@@ -6,9 +6,11 @@ import colors from './colorsDictionary';
 export default function PokemonCardInfos({pokemon}) {
   return(
     <div className="pokemon__infos" style={{background: colors[pokemon.type[0]].background }}>
-      <img src={Plus} alt="plus-img" className="pokemon__infos__plus-icon" />
-      <img src={Heart} alt="heart-img" className="pokemon__infos__heart-icon" />
-      <p className="pokemon__infos__id">#{("0000" + pokemon.id).slice(-4)}</p>
+      <div className="pokemon__infos__wrapper">
+        <img src={Plus} alt="plus-img" className="pokemon__infos__plus-icon" />
+        <img src={Heart} alt="heart-img" className="pokemon__infos__heart-icon" />
+        <p className="pokemon__infos__id">#{("0000" + pokemon.id).slice(-4)}</p>
+      </div>
       <img className="pokemon__infos__img"src={pokemon.picture} alt={pokemon.name}/>
       <p className="pokemon__infos__name">{pokemon.name}</p>
     </div>
