19118e409f489148d3c0b5485a8234b0616d03a8
Débora Teodoríco <debora.steodorico@gmail.com>
2021-11-02 16:30:09 -0300

    refactor: add grid with bootstrap

--- pokedex/src/components/Pokedex.js
+++ pokedex/src/components/Pokedex.js
@@ -3,17 +3,15 @@ import PokemonCard from './PokemonCard';
 
 export default function Pokedex({pokemons}) {
 	return (
-    <div className="container">
-      <div className="row">
-        <section className="pokemon-list">
-          {pokemons?.map((pokemon) => {
-            return  (
-              <div className="col-lg-2 col-md-3 col-sm-6">
-                <PokemonCard key={pokemon.id} pokemon={pokemon}/>
-              </div>
-            );
-          })}
-        </section>
+    <div className="container pokemon-list">
+      <div className="row gy-2 gx-2">
+        {pokemons?.map((pokemon) => {
+          return  (
+            <div className="col-lg-2 col-md-3 col-sm-6">
+              <PokemonCard key={pokemon.id} pokemon={pokemon}/>
+            </div>
+          );
+        })}
       </div>
     </div>
 	);
