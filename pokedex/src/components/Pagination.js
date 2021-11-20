f064e7ad783b0644cc7e10f329f5f144b431161f
Débora Teodoríco <debora.steodorico@gmail.com>
2021-11-20 09:48:09 -0300

    created Previous Page and Next Page buttons

--- pokedex/src/components/Pagination.js
+++ pokedex/src/components/Pagination.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 export default function Pagination() {
   return(
-    <select name="paginates" id="paginates-select">
-      <option value="10">10</option>
-      <option value="25">25</option>
-      <option value="50">50</option>
-      <option value="100">100</option>
-  </select>
+    <>
+      <select name="paginates" id="paginates-select">
+        <option value="10">10</option>
+        <option value="25">25</option>
+        <option value="50">50</option>
+        <option value="100">100</option>
+      </select>
+      <button type="button">Previous Page</button>
+      <button type="button">Next Page</button>
+  </>
   )
 }
