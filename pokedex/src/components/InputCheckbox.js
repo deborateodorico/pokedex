69c823402ede7984736d58dc44a84fb1895575bd
Débora Teodoríco <debora.steodorico@gmail.com>
2021-11-23 16:51:44 -0300

    chore: change the values from checked

--- pokedex/src/components/InputCheckbox.js
+++ pokedex/src/components/InputCheckbox.js
@@ -1,31 +1,31 @@
 import React from 'react';
 
-export default function InputCheckbox({onCheckboxWeightsChange, onCheckboxHeightsChange}) {
-const weightsOrHeights = [1,2,3,4,5];
-    return (
-      <>
-        <div>
-          <p>Weights</p>
-          {weightsOrHeights.map((weight) => {
+export default function InputCheckbox({weights, heights , onCheckboxWeightsChange, onCheckboxHeightsChange}) {
+  const weightsOrHeights = [1,2,3,4,5];
+  return (
+    <>
+      <div>
+        <p>Weights</p>
+        {weightsOrHeights.map((weight) => {
+          return(
+            <label key={weight}>
+              {weight}
+              <input type="checkbox" name="weight" value={weight} className="Input-weight" checked={weights.includes(String(weight))} onChange={onCheckboxWeightsChange}/>
+            </label>
+          )
+        })}
+      </div>
+      <div>
+        <p>Heights</p>
+          {weightsOrHeights.map((height) => {
             return(
-              <label key={weight}>
-                {weight}
-                <input type="checkbox" name="weight" value={weight} className="Input-weight" onChange={onCheckboxWeightsChange}/>
+              <label key={height}>
+                {height}
+                <input type="checkbox" name="weight" value={height} className="Input-weight" checked={heights.includes(String(height))} onChange={onCheckboxHeightsChange}/>
               </label>
             )
-          })}
-        </div>
-        <div>
-          <p>Heights</p>
-            {weightsOrHeights.map((height) => {
-              return(
-                <label key={height}>
-                  {height}
-                  <input type="checkbox" name="weight" value={height} className="Input-weight" onChange={onCheckboxHeightsChange}/>
-                </label>
-              )
-          })}
-        </div>
-      </>
-    )
+        })}
+      </div>
+    </>
+  )
 }
