c5d5dee99dbf3c51d16b10ae8c16d8a30e5638b7
Débora Teodoríco <debora.steodorico@gmail.com>
2021-11-24 16:39:05 -0300

    feat: add vscode folder with settings.json

--- pokedex/src/components/LoadingApiTypes.js
+++ pokedex/src/components/LoadingApiTypes.js
@@ -3,7 +3,10 @@ import React from 'react';
 function LoadingApiTypes() {
   return (
     <div>
-      <img src="https://c.tenor.com/0WkmuOC_W00AAAAj/waving-pikachu.gif" alt="Loading"/>
+      <img
+        src='https://c.tenor.com/0WkmuOC_W00AAAAj/waving-pikachu.gif'
+        alt='Loading'
+      />
       <p>Loading types ...</p>
     </div>
   );
