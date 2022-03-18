1ffa5f8338244a3a7b63cd98e5e78bdee380ec4b
Débora Teodoríco <debora.steodorico@gmail.com>
2022-03-18 16:30:20 -0300

    refactor: use semantic HTML

--- pokedex/src/components/detailsPageInfo/DetailsInfoAbout.js
+++ pokedex/src/components/detailsPageInfo/DetailsInfoAbout.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function DetailsInfoAbout({ about }) {
   return (
     <div className='details-about'>
-      <p className='details-about__paragraph'>About</p>
+      <h4 className='details-about__paragraph'>About</h4>
       <p className='details-about__text'>{about}</p>
     </div>
   );
