a01979b85af504e947d46ea1e6275c699624eedd
Débora Teodoríco <debora.steodorico@gmail.com>
2022-03-23 14:29:28 -0300

    feat: implement authentication

---
+++ pokedex/src/components/authentication.js
@@ -0,0 +1,83 @@
+import Cookies from 'js-cookie';
+
+const createAccount = (name, email, password) => {
+  fetch('http://pokedex.jhonnymichel.com/signup', {
+    method: 'POST',
+    mode: 'cors',
+    referrerPolicy: 'no-referrer',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify({
+      name,
+      email,
+      password,
+    }),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response.data.name, response.message);
+      Cookies.set('token', response.token);
+    });
+};
+
+const getUser = () => {
+  fetch('http://pokedex.jhonnymichel.com/profile', {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      Authorization: `Bearer ${Cookies.get('token')}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response.user.name, response.user.email);
+    });
+};
+
+const signIn = (email, password) => {
+  fetch('http://pokedex.jhonnymichel.com/signin', {
+    method: 'POST',
+    mode: 'cors',
+    referrerPolicy: 'no-referrer',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response.message);
+      Cookies.set('token', response.token);
+    });
+};
+
+const signOut = () => {
+  fetch('http://pokedex.jhonnymichel.com/signout', {
+    method: 'POST',
+    mode: 'cors',
+    referrerPolicy: 'no-referrer',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${Cookies.get('token')}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response.message);
+      Cookies.remove('token');
+    });
+};
+
+window.createAccount = createAccount;
+
+window.signIn = signIn;
+
+window.signOut = signOut;
+
+window.getUser = getUser;
